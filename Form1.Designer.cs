a7deed8d0009a267e08025366b4f13694a3e69d2
mbina <mbinagds@gmail.com>
2025-05-23 09:49:08 +0330

    add file

---
+++ Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace refund
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.cmbServiceType = new System.Windows.Forms.ComboBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtRefund = new System.Windows.Forms.TextBox();
+            this.txtuse = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dtpStartDate = new System.Windows.Forms.DateTimePicker();
+            this.dtpEndDate = new System.Windows.Forms.DateTimePicker();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtX
+            // 
+            this.txtX.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtX.Font = new System.Drawing.Font("IRYekan", 14F, System.Drawing.FontStyle.Bold);
+            this.txtX.Location = new System.Drawing.Point(13, 68);
+            this.txtX.Multiline = true;
+            this.txtX.Name = "txtX";
+            this.txtX.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtX.Size = new System.Drawing.Size(289, 49);
+            this.txtX.TabIndex = 0;
+            this.txtX.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // cmbServiceType
+            // 
+            this.cmbServiceType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbServiceType.Font = new System.Drawing.Font("IRYekan", 14F, System.Drawing.FontStyle.Bold);
+            this.cmbServiceType.FormattingEnabled = true;
+            this.cmbServiceType.Location = new System.Drawing.Point(12, 337);
+            this.cmbServiceType.Name = "cmbServiceType";
+            this.cmbServiceType.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.cmbServiceType.Size = new System.Drawing.Size(289, 53);
+            this.cmbServiceType.TabIndex = 2;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("B Lotus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnCalculate.Location = new System.Drawing.Point(12, 408);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnCalculate.Size = new System.Drawing.Size(289, 66);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "محاسبه عودت وجه";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("B Lotus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(392, 69);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(155, 49);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "کل مبلغ فاکتور";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("B Lotus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(341, 140);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(206, 49);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "تاریخ شروع سرویس";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("B Lotus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label3.Location = new System.Drawing.Point(303, 342);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(244, 49);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "مدت زمان خریداری شده";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("B Lotus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label4.Location = new System.Drawing.Point(350, 212);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label4.Size = new System.Drawing.Size(197, 49);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "تاریخ پایان سرویس";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("B Lotus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label5.Location = new System.Drawing.Point(338, 272);
+            this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label5.Size = new System.Drawing.Size(209, 49);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "مدت زمان باقی مانده";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtRefund
+            // 
+            this.txtRefund.Font = new System.Drawing.Font("IRYekan", 14F, System.Drawing.FontStyle.Bold);
+            this.txtRefund.Location = new System.Drawing.Point(13, 495);
+            this.txtRefund.Multiline = true;
+            this.txtRefund.Name = "txtRefund";
+            this.txtRefund.ReadOnly = true;
+            this.txtRefund.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtRefund.Size = new System.Drawing.Size(289, 61);
+            this.txtRefund.TabIndex = 15;
+            this.txtRefund.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtuse
+            // 
+            this.txtuse.Font = new System.Drawing.Font("IRYekan", 14F, System.Drawing.FontStyle.Bold);
+            this.txtuse.Location = new System.Drawing.Point(12, 276);
+            this.txtuse.Multiline = true;
+            this.txtuse.Name = "txtuse";
+            this.txtuse.ReadOnly = true;
+            this.txtuse.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtuse.Size = new System.Drawing.Size(289, 45);
+            this.txtuse.TabIndex = 16;
+            this.txtuse.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("B Lotus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label6.Location = new System.Drawing.Point(408, 500);
+            this.label6.Name = "label6";
+            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label6.Size = new System.Drawing.Size(139, 49);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "مبلغ بازگشتی";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("B Lotus", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label7.Location = new System.Drawing.Point(305, 501);
+            this.label7.Name = "label7";
+            this.label7.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label7.Size = new System.Drawing.Size(61, 49);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "ریال";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // dtpStartDate
+            // 
+            this.dtpStartDate.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpStartDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpStartDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpStartDate.Location = new System.Drawing.Point(13, 150);
+            this.dtpStartDate.Name = "dtpStartDate";
+            this.dtpStartDate.Size = new System.Drawing.Size(289, 34);
+            this.dtpStartDate.TabIndex = 22;
+            this.dtpStartDate.Value = new System.DateTime(2025, 3, 30, 0, 0, 0, 0);
+            // 
+            // dtpEndDate
+            // 
+            this.dtpEndDate.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpEndDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpEndDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpEndDate.Location = new System.Drawing.Point(13, 222);
+            this.dtpEndDate.Name = "dtpEndDate";
+            this.dtpEndDate.Size = new System.Drawing.Size(289, 34);
+            this.dtpEndDate.TabIndex = 23;
+            this.dtpEndDate.Value = new System.DateTime(2025, 3, 30, 0, 0, 0, 0);
+            // 
+            // btnclear
+            // 
+            this.btnclear.Font = new System.Drawing.Font("B Lotus", 16.2F, System.Drawing.FontStyle.Bold);
+            this.btnclear.Location = new System.Drawing.Point(67, 579);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(178, 43);
+            this.btnclear.TabIndex = 24;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(562, 634);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.dtpEndDate);
+            this.Controls.Add(this.dtpStartDate);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtuse);
+            this.Controls.Add(this.txtRefund);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.cmbServiceType);
+            this.Controls.Add(this.txtX);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Refund-Talashnet";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+         private System.Windows.Forms.TextBox txtX;
+         private System.Windows.Forms.ComboBox cmbServiceType;
+         private System.Windows.Forms.Button btnCalculate;
+         private System.Windows.Forms.Label label1;
+         private System.Windows.Forms.Label label2;
+         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtRefund;
+        private System.Windows.Forms.TextBox txtuse;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker dtpStartDate;
+        private System.Windows.Forms.DateTimePicker dtpEndDate;
+        private System.Windows.Forms.Button btnclear;
+    }
+}
+    
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace refund
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            cmbServiceType.Items.Add("12 = سالانه");
+            cmbServiceType.Items.Add("1 = ماهانه");
+            cmbServiceType.Items.Add("3 = سه‌ماهه");
+            cmbServiceType.Items.Add("6 = شش‌ماهه");
+            cmbServiceType.SelectedIndex = 0;
+
+            dtpStartDate.Value = DateTime.Now.AddMonths(-1); 
+            dtpEndDate.Value = DateTime.Now;
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // DateTime today = DateTime.Now;
+
+           // cmbTyearF.SelectedItem = today.Year.ToString();
+           // cmbTmonthF.SelectedItem = today.Month.ToString("D2");
+          //  cmbTdayF.SelectedItem = today.Day.ToString("D2");
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double X = Convert.ToDouble(txtX.Text);
+
+                // گرفتن نوع سرویس از کمبو باکس
+                int serviceType = Convert.ToInt32(cmbServiceType.SelectedItem.ToString().Split('=')[0].Trim());
+
+                // دریافت تاریخ شروع و پایان
+                DateTime startDate = dtpStartDate.Value;
+                DateTime finishDate = dtpEndDate.Value;
+
+                // تعداد روزهای استفاده‌شده
+                TimeSpan differenceUsed = finishDate - startDate;
+                int T_used = (int)differenceUsed.TotalDays;
+
+                // تعداد روزهای کل برای هر نوع سرویس
+                int T_total;
+                switch (serviceType)
+                {
+                    case 12:  // سالانه
+                        T_total = 365;
+                        break;
+                    case 1:  // ماهانه
+                        T_total = 30;
+                        break;
+                    case 3:  // سه‌ماهه
+                        T_total = 90;
+                        break;
+                    case 6:  // شش‌ماهه
+                        T_total = 183;
+                        break;
+                    default:
+                        MessageBox.Show("نوع سرویس نامعتبر است.");
+                        return;
+                }
+
+                // تعداد روزهای باقی‌مانده
+                int T_remaining = T_total - T_used;
+                if (T_remaining < 0)
+                {
+                    T_remaining = 0;
+                }
+
+                // نمایش روزهای باقی‌مانده
+                txtuse.Text = $"روز {T_remaining} ";
+
+                // محاسبه میزان بازپرداخت
+                double refund = CalculateRefund(X, T_used, serviceType, T_remaining);
+                txtRefund.Text = refund.ToString("N0");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("خطا در محاسبه: " + ex.Message);
+            }
+        }
+
+        private double CalculateRefund(double X, int T_used, int serviceType, int T_remaining)
+        {
+            double X_after_discount = X / 1.1;
+            double Y_after_service_discount = X_after_discount * 0.8;
+
+            int T_total = 0;
+
+            switch (serviceType)
+            {
+                case 12: // سالانه
+                    T_total = 365;
+                    break;
+                case 1: // ماهانه
+                    T_total = 30;
+                    break;
+                case 3: // سه‌ماهه
+                    T_total = 90;
+                    break;
+                case 6: // شش‌ماهه
+                    T_total = 183;
+                    break;
+                default:
+                    MessageBox.Show("نوع سرویس نامعتبر است.");
+                    return 0;
+            }
+
+            double refund = (Y_after_service_discount / T_total) * T_remaining;
+            return refund;
+        }
+
+            private void btnclear_Click(object sender, EventArgs e)
+        {
+            txtX.Clear(); 
+            txtuse.Clear(); 
+            txtRefund.Clear(); 
+
+            cmbServiceType.SelectedIndex = 0; 
+
+            dtpStartDate.Value = DateTime.Now.AddMonths(-1); 
+            dtpEndDate.Value = DateTime.Now;
+        }
+    }
+}
